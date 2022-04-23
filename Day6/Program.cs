cbe086f7d1cfadc2585edf9537225c918e6c88d5
mandara11 <mandaram2345@gmail.com>
2022-04-23 10:19:32 +0530

    Add project files.

---
+++ Day6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day6");
+            Reverse.Findreverse();
+            Console.ReadKey();
+        }
+    }
+}
